2da84a2d0a54a1c16f4326bd592ff4a98562cfeb
Renato Kempter <renato.kempter@gmail.com>
2012-05-02 22:44:37 +0200

    #5 Implemented Fourier Transform

--- src/tests/SimpleMicro.java
+++ src/tests/SimpleMicro.java
@@ -6,6 +6,9 @@ import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.DataLine;
 import javax.sound.sampled.TargetDataLine;
 
+import flanagan.complex.Complex;
+import flanagan.math.FourierTransform;
+
 public class SimpleMicro {
     public static final int RATE = 8000; //8000,11025,16000,22050,44100
 
@@ -27,19 +30,49 @@ public class SimpleMicro {
             AudioInputStream ais = new AudioInputStream(targetDataLine);
 
             // Interesting things start here
-
+            // Read Values into an array
             ais.read(values); // fills the buffer
-            doSomething(values);
+            
+            //getFourier(values);
 
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    private static void doSomething(byte[] values) {
-        // print the recorded values
-        for (int i = 0; i < RATE; i++) {
-            System.out.println(values[i]);
-        }
+    private static Complex[] getFourier(double[] sample) {
+    	
+    	FourierTransform fft = new FourierTransform();
+    	// Set Data
+    	fft.setData(sample);
+    	// Set Sampling Rate
+    	fft.setDeltaT(RATE);
+    	// Apply FFT
+        fft.transform();
+        // Get Results from FFT (Complex values)
+        Complex result[] = fft.getTransformedDataAsComplex();
+    	
+        return result;
+    }
+    
+    private static boolean determineByte(Complex frequencies[]) {
+    	
+    	/* Multiple methods possible
+    	 * 1. Highest value between 420 - 470Hz (1) or outside (0)
+    	 * 2. Define a threshold
+    	 */
+    	
+    	// Read only positive values from the frequency array (until RATE/2)
+    	
+    	return true;
+    }
+    
+    /*
+     * After 8 bits, we need to transform the byte back to
+     * an ASCII Symbol and write the symbol out
+     * (new Thread?)
+     */
+    private static void writeSymbol(byte symbol) {
+    	
     }
 }
