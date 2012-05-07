c6e0ab10c34459a8a4632c82c10c0e20463dfaf5
Renato Kempter <renato.kempter@gmail.com>
2012-05-07 23:42:46 +0200

    Created a readFile function which outputs the content of a file as a bit-array

---
+++ matlab/readFile.m
@@ -0,0 +1,18 @@
+function [bitstream] = readFile(filename)
+
+% open file
+fileID = fopen(filename);
+
+% read data into array
+data = fscanf(fid,'%s');
+
+% convert data to bytes
+bytes = unicode2native(data);
+
+% transform into 8 bit sequences
+bitstr = dec2bin(bytes,8);
+
+% create array out of the 8 bit sequences
+bitstream = str2num(reshape(bstr',[],1))';
+
+
