c3b60709bba3e4a803855862797b7086e4b42440
Renato Kempter <renato.kempter@gmail.com>
2012-05-18 11:00:08 +0200

    Bug correction

--- matlab/createMessageSignal.m
+++ matlab/createMessageSignal.m
@@ -6,9 +6,9 @@ nSamplePerBit = RATE / BPS;
 signal0 = createSoundSignal(1/BPS, 0);
 signal1 = zeros(1, 4*nSamplePerBit);
 signal1(1:1*nSamplePerBit) = createSoundSignal(1/BPS, Constants.FREQUENCY_440);
-signal1(1*nSamplePerBit:2*nSamplePerBit) = createSoundSignal(1/BPS, Constants.FREQUENCY_500);
-signal1(2*nSamplePerBit:3*nSamplePerBit) = createSoundSignal(1/BPS, Constants.FREQUENCY_600);
-signal1(3*nSamplePerBit:4*nSamplePerBit) = createSoundSignal(1/BPS, Constants.FREQUENCY_700);
+signal1(1*nSamplePerBit+1:2*nSamplePerBit) = createSoundSignal(1/BPS, Constants.FREQUENCY_500);
+signal1(2*nSamplePerBit+1:3*nSamplePerBit) = createSoundSignal(1/BPS, Constants.FREQUENCY_600);
+signal1(3*nSamplePerBit+1:4*nSamplePerBit) = createSoundSignal(1/BPS, Constants.FREQUENCY_700);
 
 nBits = length(message);
 
@@ -24,11 +24,11 @@ for i = 1:nBits
             case 0
                signal((i-1)*nSamplePerBit + 1: i*nSamplePerBit) = signal1(1:1*nSamplePerBit);
             case 1
-               signal((i-1)*nSamplePerBit + 1: i*nSamplePerBit) = signal1(1*nSamplePerBit:2*nSamplePerBit);
+               signal((i-1)*nSamplePerBit + 1: i*nSamplePerBit) = signal1(1*nSamplePerBit+1:2*nSamplePerBit);
             case 2
-               signal((i-1)*nSamplePerBit + 1: i*nSamplePerBit) = signal1(2*nSamplePerBit:3*nSamplePerBit);
+               signal((i-1)*nSamplePerBit + 1: i*nSamplePerBit) = signal1(2*nSamplePerBit+1:3*nSamplePerBit);
             case 3
-               signal((i-1)*nSamplePerBit + 1: i*nSamplePerBit) = signal1(3*nSamplePerBit:4*nSamplePerBit);
+               signal((i-1)*nSamplePerBit + 1: i*nSamplePerBit) = signal1(3*nSamplePerBit+1:4*nSamplePerBit);
         end
     end
 end
