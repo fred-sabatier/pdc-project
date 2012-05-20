e1f8912e225566848286f7323072aed06cdbad9e
Renato Kempter <renato.kempter@gmail.com>
2012-05-20 23:47:08 +0200

    ignore constant file

--- matlab/Constants.m
+++ matlab/Constants.m
@@ -10,7 +10,7 @@ classdef Constants
         INIT_DURATION = Constants.SYNCHRO_DURATION + Constants.THRESHOLD_DURATION;
         SAFETY_DURATION = 5; % Records a little longer...
         
-        N_CHAR = 16;
+        N_CHAR = 100;
         FILE_LENGTH = 8 * Constants.N_CHAR; % File lenght in bits
         
         TWO_COMPUTERS = 0;
